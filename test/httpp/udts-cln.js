7f1cad984ff0f6759e3eeaa5513d988191de16bb
tom zhou <iwebpp@gmail.com>
2020-04-21 11:32:40 -0700

    added tls srv/cln test, still failed

--- test/httpp/udts-cln.js
+++ test/httpp/udts-cln.js
@@ -1,8 +1,16 @@
 const udts = require('udts');
-const cln = udts.connect({port:51686, host: 'localhost'} , function(){
+const fs = require('fs');
+
+let options = {
+    ca: [fs.readFileSync('test-cert.pem')],
+
+    port: 51686,
+    host: "localhost",
+};
+
+const cln = udts.connect(options, function () {
     console.log('you can type char here, then server send it back:\n');
     process.stdin.resume();
-    process.stdin.pipe(cln);   
-    cln.pipe(process.stdout); 
+    process.stdin.pipe(cln);
+    cln.pipe(process.stdout);
 });
-
