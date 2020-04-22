71af37cc2a1e12d58ae6d9fdf19084ad5f239027
tom zhou <iwebpp@gmail.com>
2020-04-21 22:58:10 -0700

    added tls srv/cln case

---
+++ test/httpp/tls-srv.js
@@ -0,0 +1,15 @@
+const tls = require('tls');
+let   fs   = require('fs');
+
+var options = {
+     key: fs.readFileSync('test-key.pem'),
+    cert: fs.readFileSync('test-cert.pem')
+};
+
+const srv = tls.createServer(function(socket){
+    socket.pipe(socket);     
+});
+
+srv.listen(51686);
+console.log('Listening on TCP port 51686');
+
